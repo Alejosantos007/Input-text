6301a8614dc29c154b66fcbeee7a1f54b8e60d94
AlejoSantos007 <alejosantos.2024@gmail.com>
2025-05-26 14:10:23 -0300

    BOTON DELETED

--- js/script.js
+++ js/script.js
@@ -4,5 +4,6 @@ let mostranding = document.getElementById('mostranding');
 inputing.addEventListener('keydown', function (e) {
     if (e.key === "Enter") {
         mostranding.innerText = inputing.value;
+        
     }
 });
