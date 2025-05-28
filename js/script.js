d7133ee9eb288139bfe98613c1eb5b8ee65baa64
AlejoSantos007 <alejosantos.2024@gmail.com>
2025-05-28 19:47:40 -0300

    COSITAS SE VINIERON

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-let inputing = document.getElementById('inputing');
-let mostranding = document.getElementById('mostranding');
-
-inputing.addEventListener('keydown', function (e) {
-    if (e.key === "Enter") {
-        mostranding.innerText = inputing.value;
-        
+var user = [];
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let confirmar = document.getElementById('confirmar');
+let textote = document.getElementById('textote');
+textote.style.backgroundColor = 'greenyellow';
+textote.style.color = 'black';
+textote.style.borderRadius= '5px';
+    function cosa () {
+    if (user.value !== "" && password.value !== "") {
+        textote.innerText = 'CUENTA CREADA';
+    } else {
+        textote.innerText = 'FALTA METER INFO';
     }
-});
+    user2 = username.value;
+pass = password.value;
+user.push ({user: user2, password: pass});
+document.getElementById('username').value = '';
+document.getElementById('password').value = '';
+};
